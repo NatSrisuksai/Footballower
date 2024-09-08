245688928b52c9163089b89b92071594bdb808ac
NatSrisuksai <natsrisuksai02@gmail.com>
2024-09-08 14:54:21 +0700

    fix : more proper <Route>

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
-import './App.css'
-import HomePage from "./components/HomePage"
-import AboutUs from "./components/about-us/AboutUs"
-import ProgramPage from "./components/Program-Page/ProgramPage"
+import "./App.css";
+import HomePage from "./components/HomePage";
+import AboutUs from "./components/about-us/AboutUs";
+import ProgramPage from "./components/Program-Page/ProgramPage";
 import {
   BrowserRouter as Router,
   Routes,
@@ -11,20 +11,15 @@ import {
 } from "react-router-dom";
 
 function App() {
-
   return (
     <Router>
       <Routes>
-        <Route exact  path="/" element={<HomePage />}/>
-      </Routes>
-      <Routes>
-        <Route exact  path="about-us" element={<AboutUs />}/>
-      </Routes>
-      <Routes>
-        <Route exact  path="program-page" element={<ProgramPage />}/>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/about-us" element={<AboutUs />} />
+        <Route exact path="/program-page" element={<ProgramPage />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
